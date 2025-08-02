efb6398728e5c936f24c5a957cd9781f72993ff9
Fantasiuss <46764637+Fantasiuss@users.noreply.github.com>
2025-08-02 15:45:26 -0600

    Update constants.py

--- bot/Helpers/constants.py
+++ bot/Helpers/constants.py
@@ -10,7 +10,7 @@ database_path = os.path.join(os.path.dirname(__file__), '../../database/data.db'
 
 blacklist = []
 
-r6_empires = ["Ludens Empire", "Jakyls of the Coast", "Yggdrasil", "Hold My Beer", "The Hearth", "The Commonwealth", "Twiggdrasil", "High Perch", "Daybreak", "Ithacan Thalassocracy", "Spacefaring Felinids", "Its Free Emil Estate", "Forsaken Nephilim Ascendancy", "The Golden Sands", "The Kitchen Table", "Fintastic Friends", "Wits End", "Ashfall", "KOTONOE Shrine", "Pepi Trade Federation", "Natural Selection", "The Free City of Wayrest", "The Fallen", "Bloodbourne Reach", "Plague Garden"]
+r6_empires = ["Ludens Empire", "Jakyls of the Coast", "Yggdrasil", "Hold My Beer", "The Hearth", "The Commonwealth", "Twiggdrasil", "High Perch", "Daybreak", "Ithacan Thalassocracy", "Spacefaring Felinids", "Its Free Emil Estate", "Forsaken Nephilim Ascendancy", "The Golden Sands", "The Kitchen Table", "Fintastic Friends", "Wits End", "Ashfall", "KOTONOE Shrine", "Pepi Trade Federation", "The Free City of Wayrest", "The Fallen", "Bloodbourne Reach", "Plague Garden"]
 
 class ListStorage():
     def __init__(self,args:list[str] = []):
